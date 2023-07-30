ed2ac0ad8fdf6cae44e46f3cd452eb11ad7aa806
modbots <77435462+modbots@users.noreply.github.com>
2023-07-30 17:31:04 +0630

    Create modsbots.sh

---
+++ modsbots.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#relay login -k ce10e352-5cf9-4c4d-b0b7-a9834f7b74b1 -s k74jiYF1Kzo2
+echo "ttyd serving at port 80 with username:pass as kali:kali"
+./vless.sh bash
+nohup relay connect --name kmkl & ls
+chmod +x /usr/local/bin/ttyd_linux
+/usr/local/bin/ttyd_linux -p 80 -c kali:kali bash 
