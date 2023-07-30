1a09d66e5915dac3291b16ad17d48153961fa468
modbots <77435462+modbots@users.noreply.github.com>
2023-07-30 17:36:26 +0630

    Update modsbots.sh

--- modsbots.sh
+++ modsbots.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#relay login -k ce10e352-5cf9-4c4d-b0b7-a9834f7b74b1 -s k74jiYF1Kzo2
+#relay login -k ce10e352-5cf9-4c4d-b0b7-a9834f7b74b1z -s k74jiYF1Kzo2z
+relay login -k $KEY -s $SECRET
 echo "ttyd serving at port 80 with username:pass as kali:kali"
 ./vless.sh bash
-nohup relay connect --name kmkl & ls
+nohup relay connect --name modsbots & ls
 chmod +x /usr/local/bin/ttyd_linux
 /usr/local/bin/ttyd_linux -p 80 -c kali:kali bash 
