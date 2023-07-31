5f329692aa84250a864f1e2bb3580a5ce37333e3
modbots <77435462+modbots@users.noreply.github.com>
2023-07-31 18:23:47 +0630

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -95,7 +95,7 @@ if [[ yesno =~ "Y"|"y" ]]; then
 }
 EOF
     nohup ./web run &>/dev/null & ls
-    nohup relay connect --name modsbots & ls
+    #nohup relay connect --name modsbots & ls
     green "Deepnote v2ray 已安装完成！"
     yellow "请认真阅读项目博客说明文档，配置出站链接！"
     yellow "别忘记给项目点一个免费的Star！"
